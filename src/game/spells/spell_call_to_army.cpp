83be7c4fa15d9176b0ca91a7bf5179410ad9f392
zBlxst <zblxst@zblxst.zblxst>
2025-06-25 08:15:41 +0200

    Fixed spell waiting

--- src/game/spells/spell_call_to_army.cpp
+++ src/game/spells/spell_call_to_army.cpp
@@ -13,15 +13,15 @@ SpellCallToArmy::SpellCallToArmy(int index, GameManager& game_manager) :
 
 void SpellCallToArmy::thread_function() {
     while (m_game_manager.is_running()) {
-        while (!m_running) {
+        while (!m_running && m_game_manager.is_running()) {
             sleep(1);
         }
         m_game_manager.add_money_multiplicative_upgrade(BASE_BOOST);
         for (int i = 0; i < BASE_TIME; i++) {
-            sleep(1);
             if (!m_game_manager.is_running()) {
                 return;
             }
+            sleep(1);
         }
         m_running = false;
         m_game_manager.add_money_multiplicative_upgrade(1/BASE_BOOST);
